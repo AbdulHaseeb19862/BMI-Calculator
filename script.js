c60ddcbd3563ae61d18092c31baa0b2eab310fa1
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-10-07 16:15:54 +0500

    set condition on calculate and display result on condition

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ calculateBtn.addEventListener("click", function () {
   const height = document.getElementById("height").value;
   const weight = document.getElementById("weight").value;
   const result = document.getElementById("result");
-  var BMI = Number(weight / height);
-  result.textContent = BMI;
+  const 
+  var BMI = (weight / (height * height)).toFixed(2);
+  if (BMI < 18.5) {
+    result.textContent = `Your BMI value is : ${BMI} and your BMI result is Underweight`;
+  } else if (BMI >= 18.5 && BMI < 24.9) {
+    result.textContent = `Your BMI value is : ${BMI} and your BMI result is Normal weight`;
+  } else if (BMI >= 25 && BMI < 29.9) {
+    result.textContent = `Your BMI value is : ${BMI} and your BMI result is Overweight`;
+  } else if (BMI >= 30) {
+    result.textContent = `Your BMI value is : ${BMI} and your BMI result is Obesity`;
+  }
 });
