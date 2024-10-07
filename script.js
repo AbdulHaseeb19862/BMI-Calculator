1afdcb63716bffa22a474230b0e9f7a86e1cc555
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-10-07 17:37:29 +0500

    Add functionality to reset button

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const calculateBtn = document.getElementById("calculateBtn");
+const resetBtn = document.getElementById("resetBtn");
 calculateBtn.addEventListener("click", function () {
   const height = document.getElementById("height").value;
   const weight = document.getElementById("weight").value;
+  if (!height || !weight) {
+    return alert("Both Fields are Requirds");
+  }
+
   const result = document.getElementById("result");
-  const 
+  const outputContainer = document.getElementById("outputContainer");
+  outputContainer.style.display = "block";
+  outputContainer.style.display = "flex";
+  outputContainer.style.justifyContent = "center";
+  outputContainer.style.alignItems = "center";
+
   var BMI = (weight / (height * height)).toFixed(2);
   if (BMI < 18.5) {
     result.textContent = `Your BMI value is : ${BMI} and your BMI result is Underweight`;
@@ -14,4 +24,16 @@ calculateBtn.addEventListener("click", function () {
   } else if (BMI >= 30) {
     result.textContent = `Your BMI value is : ${BMI} and your BMI result is Obesity`;
   }
+});
+
+resetBtn.addEventListener("click", function () {
+  const height = document.getElementById("height");
+  const weight = document.getElementById("weight");
+  const result = document.getElementById("result");
+  const outputContainer = document.getElementById("outputContainer");
+
+  height.value = "";
+  weight.value = "";
+  result.textContent = "";
+  outputContainer.style.display = "none";
 });
